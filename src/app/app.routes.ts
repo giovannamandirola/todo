aa144c3ee9b378d72394f3e3250e0f49c56d5ae9
Giovanna Mandirola <GiovannaMandirola@IBM-PF3HS9DC>
2024-12-03 20:54:28 -0300

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { HealthComponent } from './pages/health/health.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'health', component: HealthComponent}
+];
